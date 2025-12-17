a9ac7aa39848d8d6293d3551375208bb795365b0
omera71 <b2b.iwan@gmail.com>
2025-12-17 09:10:20 +0700

    Update Day02.py

--- Day02.py
+++ Day02.py
@@ -1,14 +1,21 @@
-print("Welcome to Day 2!")
-print("What was the total bill?")
-total_bill = float(input());
 
-print("What percentage tip would you like to give? 10, 12, or 15?")
-tip_percentage = int(input());
-tip = (tip_percentage / 100) * total_bill;
-total_with_tip = total_bill + tip;
-print(f"The total bill including a {tip_percentage}% tip is: ${total_with_tip:.2f}")
 
-print("How many people to split the bill?")
-num_people = int(input());
-bill_per_person = total_with_tip / num_people;
-print(f"Each person should pay: ${bill_per_person:.2f}")
+
+
+# print("Welcome to Day 2!")
+# print("What was the total bill?")
+# total_bill = float(input());
+
+# print("What percentage tip would you like to give? 10, 12, or 15?")
+# tip_percentage = int(input());
+# tip = (tip_percentage / 100) * total_bill;
+# total_with_tip = total_bill + tip;
+# print(f"The total bill including a {tip_percentage}% tip is: ${total_with_tip:.2f}")
+
+# print("How many people to split the bill?")
+# num_people = int(input());
+# bill_per_person = total_with_tip / num_people;
+# print(f"Each person should pay: ${bill_per_person:.2f}")
+
+# mystery = 734_529.678;
+# print(f"Mystery number is: {mystery:,}")
